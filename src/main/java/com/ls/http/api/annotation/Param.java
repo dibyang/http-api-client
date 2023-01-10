83de00d8a454d10068cd07e22966bd52ab8953e4
yangzj <yangzj@leofs.com.cn>
2023-01-10 14:05:46 +0800

    async support

--- src/main/java/com/ls/http/api/annotation/Param.java
+++ src/main/java/com/ls/http/api/annotation/Param.java
@@ -5,8 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * jdk8 设置 javac -parameters  方法参数名和请求参数名一致时可以省略该注解
+ *
+ * gradle 设置如下
+ *
+ * tasks.withType(JavaCompile) {
+ *   options.compilerArgs << "-parameters"
+ * }
+ *
+ */
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Param {
-  public String value();
+  String value();
 }
