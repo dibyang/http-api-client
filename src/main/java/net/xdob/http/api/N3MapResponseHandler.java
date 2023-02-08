1f973cd359f967c665a9d55ebe7bd64015d01368
yangzj <yangzj@leofs.com.cn>
2023-02-08 09:57:00 +0800

    修复N3MapResponseHandler类型处理的bug

--- src/main/java/net/xdob/http/api/N3MapResponseHandler.java
+++ src/main/java/net/xdob/http/api/N3MapResponseHandler.java
@@ -13,24 +13,43 @@ public class N3MapResponseHandler extends AbstractResponseHandler<N3Map> {
 
   public static final String APPLICATION_COMPLEX = "application/complex";
   public static final String RETURN__ = "__RETURN__";
+  public static final String CONTENT_TYPE = "ContentType";
 
   @Override
   public N3Map handleBody(SimpleBody body) throws IOException, ParseException {
     N3Map data = new N3Map();
     ContentType contentType =body.getContentType();
     if(contentType==null){
-      contentType = ContentType.DEFAULT_BINARY;
+      contentType = ContentType.APPLICATION_JSON;
     }
-    if(ContentType.APPLICATION_JSON.getMimeType().equals(contentType.getMimeType())){
+    data.put(CONTENT_TYPE,contentType.getMimeType());
+    if(ContentType.APPLICATION_JSON.isSameMimeType(contentType)){
       String json = body.getBodyText();
       data.putAll(Jsons.i.fromJson(json,N3Map.class));
-    }else if(APPLICATION_COMPLEX.equals(contentType.getMimeType())){
+    }else if(ContentType.DEFAULT_TEXT.isSameMimeType(contentType)){
       String json = body.getBodyText();
       data.putAll(Jsons.i.fromJson(json,N3Map.class));
-    }else{
-
+    }else if(ContentType.DEFAULT_BINARY.isSameMimeType(contentType)
+        ||ContentType.APPLICATION_OCTET_STREAM.isSameMimeType(contentType)
+        ||ContentType.APPLICATION_PDF.isSameMimeType(contentType)){
+      byte[] buffer = body.getBodyBytes();
+      data.put(CONTENT_TYPE,contentType.getMimeType());
+      data.put(RETURN__,buffer);
+    }else if(contentType.getMimeType().endsWith("json")){
+      String json = body.getBodyText();
+      data.putAll(Jsons.i.fromJson(json,N3Map.class));
+    }else if(contentType.getMimeType().endsWith("xml")){
+      String xml = body.getBodyText();
+      data.put(RETURN__,xml);
+    }else if(contentType.getMimeType().startsWith("image/")){
       byte[] buffer = body.getBodyBytes();
       data.put(RETURN__,buffer);
+    }else if(contentType.getMimeType().startsWith("text/")){
+      String text = body.getBodyText();
+      data.put(RETURN__,text);
+    }else{
+      String text = body.getBodyText();
+      data.put(RETURN__,text);
     }
     return data;
   }
