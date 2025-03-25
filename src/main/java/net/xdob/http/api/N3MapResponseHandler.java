74e0ef0ee0336b3467bb589191d1acfb589259d9
yangzj <yangzj@leofs.com.cn>
2025-03-25 16:15:27 +0800

    升级依赖包，支持任意返回类型

--- src/main/java/net/xdob/http/api/N3MapResponseHandler.java
+++ src/main/java/net/xdob/http/api/N3MapResponseHandler.java
@@ -5,11 +5,14 @@ import com.ls.luava.common.N3Map;
 import org.apache.hc.client5.http.async.methods.SimpleBody;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import java.io.IOException;
 
 public class N3MapResponseHandler extends AbstractResponseHandler<N3Map> {
+  static final Logger LOG = LoggerFactory.getLogger(N3MapResponseHandler.class);
 
   public static final String APPLICATION_COMPLEX = "application/complex";
   public static final String RETURN__ = "__RETURN__";
@@ -18,39 +21,42 @@ public class N3MapResponseHandler extends AbstractResponseHandler<N3Map> {
   @Override
   public N3Map handleBody(SimpleBody body) throws IOException, ParseException {
     N3Map data = new N3Map();
-    ContentType contentType =body.getContentType();
-    if(contentType==null){
+    ContentType contentType = body.getContentType();
+    if (contentType == null) {
       contentType = ContentType.APPLICATION_JSON;
     }
-    data.put(CONTENT_TYPE,contentType.getMimeType());
-    if(ContentType.APPLICATION_JSON.isSameMimeType(contentType)){
-      String json = body.getBodyText();
-      data.putAll(Jsons.i.fromJson(json,N3Map.class));
-    }else if(ContentType.DEFAULT_TEXT.isSameMimeType(contentType)){
-      String json = body.getBodyText();
-      data.putAll(Jsons.i.fromJson(json,N3Map.class));
-    }else if(ContentType.DEFAULT_BINARY.isSameMimeType(contentType)
-        ||ContentType.APPLICATION_OCTET_STREAM.isSameMimeType(contentType)
-        ||ContentType.APPLICATION_PDF.isSameMimeType(contentType)){
+    data.put(CONTENT_TYPE, contentType.getMimeType());
+    if (ContentType.APPLICATION_JSON.isSameMimeType(contentType)) {
+      data.put(RETURN__, getReturn(body));
+    } else if (ContentType.DEFAULT_TEXT.isSameMimeType(contentType)) {
+      data.put(RETURN__, getReturn(body));
+    } else if (ContentType.DEFAULT_BINARY.isSameMimeType(contentType)
+        || ContentType.APPLICATION_OCTET_STREAM.isSameMimeType(contentType)
+        || ContentType.APPLICATION_PDF.isSameMimeType(contentType)) {
       byte[] buffer = body.getBodyBytes();
-      data.put(CONTENT_TYPE,contentType.getMimeType());
-      data.put(RETURN__,buffer);
-    }else if(contentType.getMimeType().endsWith("json")){
-      String json = body.getBodyText();
-      data.putAll(Jsons.i.fromJson(json,N3Map.class));
-    }else if(contentType.getMimeType().endsWith("xml")){
+      data.put(CONTENT_TYPE, contentType.getMimeType());
+      data.put(RETURN__, buffer);
+    } else if (contentType.getMimeType().endsWith("json")) {
+      data.put(RETURN__, getReturn(body));
+    } else if (contentType.getMimeType().endsWith("xml")) {
       String xml = body.getBodyText();
-      data.put(RETURN__,xml);
-    }else if(contentType.getMimeType().startsWith("image/")){
+      data.put(RETURN__, xml);
+    } else if (contentType.getMimeType().startsWith("image/")) {
       byte[] buffer = body.getBodyBytes();
-      data.put(RETURN__,buffer);
-    }else if(contentType.getMimeType().startsWith("text/")){
+      data.put(RETURN__, buffer);
+    } else if (contentType.getMimeType().startsWith("text/")) {
       String text = body.getBodyText();
-      data.put(RETURN__,text);
-    }else{
+      data.put(RETURN__, text);
+    } else {
       String text = body.getBodyText();
-      data.put(RETURN__,text);
+      data.put(RETURN__, text);
     }
     return data;
+  }
+
+  private Object getReturn(SimpleBody body) {
+    String json = body.getBodyText();
+    //LOG.info("getReturn:{}", o);
+    return Jsons.i.fromJson(json, Object.class);
   }
 }
